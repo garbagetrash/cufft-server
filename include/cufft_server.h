23a3215129302b2b067670866350645ba181ec36
Kyle Harris <kaharrs2@gmail.com>
2024-10-05 22:10:43 -0600

    failing to build

--- include/cufft_server.h
+++ include/cufft_server.h
@@ -1,9 +1,12 @@
 #ifndef __CUFFT_SERVER_H__
 #define __CUFFT_SERVER_H__
 
+extern "C" {
 
 // Prototypes
 void c2c_fft_batch(float *h_data, int nfft, int batch);
+
+}
 
 
 #endif // __CUFFT_SERVER_H__
